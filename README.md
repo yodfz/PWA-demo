b6013470974800a49089daf918650460af4e0216
yodfz <yodfz@qq.com>
2018-02-28 15:54:13 +0800

    manifest文件添加,icon图片来源网络

---
+++ README.md
@@ -0,0 +1,6 @@
+运行
+```
+node server.js
+```
+
+打开浏览器输入`http://localhost:8080`
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // 注册 service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(function (registration) {
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
         // 注册成功
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(function (err) {
